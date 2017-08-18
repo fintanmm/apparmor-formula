208aaf8dca122b86f1f504a95ce7eed63e5174da
Fintan MacMahon <fintan.macmahon@gmail.com>
2017-08-18 12:39:29 +0100

    Rearrange test order

--- test/integration/default/testinfra/test_apparmor.py
+++ test/integration/default/testinfra/test_apparmor.py
@@ -1,15 +1,15 @@
 import testinfra
 
 
+def test_apparmor_is_installed(host):
+    apparmor = host.package("apparmor")
+    assert apparmor.is_installed
+
+
 def test_apparmor_running_and_enabled(host):
     apparmor = host.service("apparmor")
     assert apparmor.is_running
     assert apparmor.is_enabled
-
-
-def test_apparmor_is_installed(host):
-    apparmor = host.package("apparmor")
-    assert apparmor.is_installed
 
 
 def test_config_file(host):
