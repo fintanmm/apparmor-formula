3df9dcaee0db5cef42cb87e45ff89297955cc7da
Fintan MacMahon <fintan.macmahon@gmail.com>
2017-08-18 13:46:35 +0100

    Removed varialbe for check_output

--- test/integration/default/testinfra/test_complain.py
+++ test/integration/default/testinfra/test_complain.py
@@ -3,6 +3,6 @@ import testinfra
 
 def test_apparmor_complain_profile(host):
     apparmor = host.file("/etc/apparmor.d/usr.sbin.rsyslog")
-    status = apparmor.check_output("aa-status > /apparmor-output")
+    apparmor.check_output("aa-status > /apparmor-output")
     status_file = host.file("/apparmor-output")
     assert status_file.contains("1 profiles are in complain mode")
