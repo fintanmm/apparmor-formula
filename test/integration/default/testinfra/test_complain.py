7e6676beb961d6827b02c02a39fd2503a9e43b7c
Fintan MacMahon <fintan.macmahon@gmail.com>
2017-08-18 13:45:49 +0100

    Out aa-status to a file then check the contents of that file.

--- test/integration/default/testinfra/test_complain.py
+++ test/integration/default/testinfra/test_complain.py
@@ -3,4 +3,6 @@ import testinfra
 
 def test_apparmor_complain_profile(host):
     apparmor = host.file("/etc/apparmor.d/usr.sbin.rsyslog")
-    assert apparmor.check_output("aa-status", "1 profiles are in complain mode")
+    status = apparmor.check_output("aa-status > /apparmor-output")
+    status_file = host.file("/apparmor-output")
+    assert status_file.contains("1 profiles are in complain mode")
