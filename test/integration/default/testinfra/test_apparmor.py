7ae344a42a649faa95b63e7c31fe11dc4004a8ff
Fintan MacMahon <fintan.macmahon@gmail.com>
2017-08-18 12:39:06 +0100

    Add test to see if apparmor is running

--- test/integration/default/testinfra/test_apparmor.py
+++ test/integration/default/testinfra/test_apparmor.py
@@ -1,6 +1,12 @@
 import testinfra
 
 
+def test_apparmor_running_and_enabled(host):
+    apparmor = host.service("apparmor")
+    assert apparmor.is_running
+    assert apparmor.is_enabled
+
+
 def test_apparmor_is_installed(host):
     apparmor = host.package("apparmor")
     assert apparmor.is_installed
