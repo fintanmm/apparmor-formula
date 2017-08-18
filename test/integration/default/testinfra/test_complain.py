f67b128115bf5fe51bc328d53f6481c2be3fc109
Fintan MacMahon <fintan.macmahon@gmail.com>
2017-08-18 13:05:51 +0100

    Check the output of aa-status for results

--- test/integration/default/testinfra/test_complain.py
+++ test/integration/default/testinfra/test_complain.py
@@ -3,4 +3,4 @@ import testinfra
 
 def test_apparmor_complain_profile(host):
     apparmor = host.file("/etc/apparmor.d/usr.sbin.rsyslog")
-    assert apparmor.contains("complain")
+    assert apparmor.check_output("aa-status", "1 profiles are in complain mode")
