c418b422521cd6c1eeeafe9d433e0f0160ebd775
Fintan MacMahon <fintan.macmahon@gmail.com>
2017-08-18 15:02:18 +0100

    Use host instead of Command

--- test/integration/default/testinfra/test_complain.py
+++ test/integration/default/testinfra/test_complain.py
@@ -2,5 +2,5 @@ import testinfra
 
 
 def test_apparmor_complain_profile(host):
-    aa_status = Command.check_output("aa-status")
+    aa_status = host.check_output("aa-status")
     assert "1 profiles are in complain mode" in aa_status
